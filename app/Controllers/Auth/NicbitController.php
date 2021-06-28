79131c887919847ed3851ae741d1246a0ec614b8
Flavio Lima <bhlima2@gmail.com>
2021-06-28 03:54:55 -0300

    Insert Header Info all files

--- app/Controllers/Auth/NicbitController.php
+++ app/Controllers/Auth/NicbitController.php
@@ -2,16 +2,38 @@
 namespace App\Controllers\Auth;
 
 /**
- * Class BaseController
+ * nicbit isp
  *
- * BaseController provides a convenient place for loading components
- * and performing functions that are needed by all your controllers.
- * Extend this class in any new controllers:
- *     class Home extends BaseController
+ * An open source application management for isp - Radius / Mikrotik
  *
- * For security be sure to declare any new methods as protected or private.
+ * This content is released under the MIT License (MIT)
  *
- * @package CodeIgniter
+ * Copyright (c) 2021-2021 - nicbit 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package    ISP - Nicbit 
+ * @author     nicbit dev team @ Flavio Lima
+ * @copyright  2021-2021 Nicbit.com 
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @since      Version 1.1.0
+ * @filesource
  */
 
 use CodeIgniter\Controller;
